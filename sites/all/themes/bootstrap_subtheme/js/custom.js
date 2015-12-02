2f49095713263b65ed373bad2eb0c8d49203f927
Massimo Moro <mayoita@hotmail.com>
2015-12-02 16:42:30 +0100

    Map behaviors

--- sites/all/themes/bootstrap_subtheme/js/custom.js
+++ sites/all/themes/bootstrap_subtheme/js/custom.js
@@ -46,3 +46,84 @@
     });
 
 })(jQuery, Drupal, this, this.document);
+
+(function ($) {
+    Drupal.behaviors.loadElaboMap = {
+        attach: function (context, settings) {
+
+            var apikey = Drupal.settings.custom_variable.apikey;
+            console.log(apikey);
+            alert(apikey);
+
+            // Load settings
+            var lat_center = Drupal.settings.rsv_casino_map.latitude;
+            var lon_center = Drupal.settings.rsv_casino_map.longitude;
+            var zoomlevel =  Drupal.settings.rsv_casino_map.zoom;
+
+            var lat_a = Drupal.settings.rsv_casino_map.latitude_a;
+            var lon_a = Drupal.settings.rsv_casino_map.longitude_a;
+
+            var lat_b = Drupal.settings.rsv_casino_map.latitude_b;
+            var lon_b = Drupal.settings.rsv_casino_map.longitude_b;
+
+            var icons_path = Drupal.settings.rsv_casino_map.icons_path;
+            var icon_a = Drupal.settings.rsv_casino_map.icon_a;
+            var icon_b = Drupal.settings.rsv_casino_map.icon_b;
+
+            var name_a = Drupal.settings.rsv_casino_map.name_a;
+            var name_b = Drupal.settings.rsv_casino_map.name_b;
+
+            var markers =  [
+                [name_a, lat_a, lon_a, icon_a],
+                [name_b, lat_b, lon_b, icon_b],
+            ];
+
+
+            // Generate map
+
+            // Center coords
+            var latlng_center = new google.maps.LatLng(lat_center,lon_center);
+
+            // Place a coords
+            var latlng_a = new google.maps.LatLng(lat_a,lon_a);
+
+            // Place b coords
+            var latlng_b = new google.maps.LatLng(lat_b,lon_b);
+
+            var myOptions = {
+                zoom: zoomlevel,
+                center: latlng_center,
+                scrollwheel: false,
+                panControl: false,
+                zoomControl: false,
+                mapTypeControl:false,
+                scaleControl: false,
+                streetViewControl: false,
+                overviewMapControl: false,
+                draggable: false,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+            var marker, i;
+
+            for (i = 0; i < markers.length; i++) {
+                marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(markers[i][1], markers[i][2]),
+                    map: map,
+                    icon: icons_path + markers[i][3],
+                    title: markers[i][0]
+                });
+
+                google.maps.event.addListener(marker, 'click', function(){
+                    var infowindow = new google.maps.InfoWindow({
+                        content: this.title
+                    });
+                    infowindow.open(map, this);
+                });
+
+            }
+
+        }
+    }
+}(jQuery));
